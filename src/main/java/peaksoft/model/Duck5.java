1da02292f687c8d49d0136d55318cb20fc4cc83c
ArzymatovaNurperi <arzimatovanurperi@gmail.com>
2021-07-15 17:37:02 +0300

    init

--- src/main/java/peaksoft/model/Duck5.java
+++ src/main/java/peaksoft/model/Duck5.java
@@ -1,12 +1,16 @@
 package peaksoft.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
 
+    @Autowired
+    private Egg6 egg6;
+
     @Override
     public String toString() {
-        return ", в утке яйцо " + "";
+        return ", в утке яйцо " + egg6.toString();
     }
 }
--- src/main/java/peaksoft/model/Egg6.java
+++ src/main/java/peaksoft/model/Egg6.java
@@ -1,9 +1,10 @@
 package peaksoft.model;
 
 public class Egg6 {
+    private Needle7 needle7;
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + ""+needle7.toString();
     }
 }
